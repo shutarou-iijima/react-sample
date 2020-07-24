6cea4b393a0ed992d815245038c57322752c72df
shutarou-iijima <s.iijima@quo-digital.jp>
2020-07-24 17:52:52 +0900

    rename file: Title.tsx -> Header.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from "./components/Title";
+import Header from "./components/Header";
 
 ReactDOM.render(
     <div className="twitter">

62ab82247cbe9ed930b366703f79a21da025dc16
shutarou-iijima <s.iijima@quo-digital.jp>
2020-07-24 18:00:45 +0900

    add menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Menu() {
+    return(
+        <div className="menu">
+            <ul className="menu__container">
+                <li className="menu__item">タイムライン</li>
+                <li className="menu__item">プロフィール</li>
+                <li className="menu__item">このサイトについて</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from "./components/Header";
+import Menu from "./components/Menu";
 
 ReactDOM.render(
     <div className="twitter">
         <Header />
+        <Menu />
     </div>,
     document.getElementById('root')
 );

d19237fdaa1b18039e0a56db880737ccb2d586da
shutarou-iijima <s.iijima@quo-digital.jp>
2020-07-26 16:04:09 +0900

    文字数制限140

--- src/components/TweetArea.tsx
+++ src/components/TweetArea.tsx
@@ -5,17 +5,27 @@ interface Props {
 }
 
 function TweetArea({ onClick }: Props) {
-    const [text, setText] = useState('aiueo')
+    const [text, setText] = useState('')
+    const [count, setCount] = useState(0)
 
     return (
         <div className="tweetarea">
+            <div>
+                {count} / 140
+            </div>
             <div>
                 <textarea
                     className="tweetarea__textarea"
                     value={text}
                     onChange={ (event) => {
+                        const value = event.target.value
+                        if (value.length > 140) {
+                            return
+                        }
+                        setCount(event.target.value.length)
                         setText(event.target.value)
                     }}
+                    placeholder="何を呟く？"
                 />
             </div>
             <div>
@@ -24,6 +34,7 @@ function TweetArea({ onClick }: Props) {
                     className="tweetarea__submit"
                     onClick={ () => {
                         onClick(text)
+                        setText('')
                     }}
                 >
                     呟く

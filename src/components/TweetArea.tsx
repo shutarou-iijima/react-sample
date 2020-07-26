1f5524556da85da144f628b29d752f59e0bab5d0
shutarou-iijima <s.iijima@quo-digital.jp>
2020-07-26 15:11:06 +0900

    ツイートできるように

--- src/components/TweetArea.tsx
+++ src/components/TweetArea.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-function TweetArea() {
+interface Props {
+    onClick: (value: string) => void
+}
+
+function TweetArea({ onClick }: Props) {
+    const [text, setText] = useState('aiueo')
 
     return (
         <div className="tweetarea">
             <div>
-                <textarea className="tweetarea__textarea" />
+                <textarea
+                    className="tweetarea__textarea"
+                    value={text}
+                    onChange={ (event) => {
+                        setText(event.target.value)
+                    }}
+                />
             </div>
             <div>
-                <button type="button" className="tweetarea__submit">呟く</button>
+                <button
+                    type="button"
+                    className="tweetarea__submit"
+                    onClick={ () => {
+                        onClick(text)
+                    }}
+                >
+                    呟く
+                </button>
             </div>
         </div>
     )
--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import TweetArea from "../components/TweetArea";
 import TweetCard from "../components/TweetCard";
 
+interface Tweet {
+    id: number
+    name: string
+    value: string
+}
+
 function Timeline() {
+    const [tweets, setTweets] = useState<Tweet[]>([
+        { id: 3, name: '太郎', value: "hello"},
+        { id: 2, name: '次郎', value: "あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお"},
+        { id: 1, name: '三郎', value: "void(0)と書いていた歴史的な理由がありましたが、現在では使用されなくなっています。より魅力的なページ作成のためには最新のコーディングのトレンドにも精通していなければなりませんね。コーディングのトレンドに歴史があることを知ると、どのように書くべきなのかがより一層わかるようになります。"},
+    ])
+
+    const tweetCards = [...tweets]
+        .sort((a, b) => {
+            return b.id - a.id
+        })
+        .map(tweet => {
+            return <TweetCard name={tweet.name} value={tweet.value} key={tweet.id}/>
+        })
 
     return (
         <div className="timeline">
-            <TweetArea />
+            <TweetArea
+                onClick={ (value) => {
+                    const id = Math.max(...tweets.map(tweet => tweet.id)) + 1
+                    const tweet = {
+                        id: id,
+                        name: '名無しの権兵衛',
+                        value: value,
+                    }
+                    const newTweets = [...tweets].concat(tweet)
+                    setTweets(newTweets)
+                }}
+            />
             <div className="tweetcard__container">
-                <TweetCard name="太郎" value="hello" />
-                <TweetCard name="次郎" value="あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお" />
-                <TweetCard name="三郎" value="void(0)と書いていた歴史的な理由がありましたが、現在では使用されなくなっています。より魅力的なページ作成のためには最新のコーディングのトレンドにも精通していなければなりませんね。コーディングのトレンドに歴史があることを知ると、どのように書くべきなのかがより一層わかるようになります。" />
+                {tweetCards}
             </div>
         </div>
     )
